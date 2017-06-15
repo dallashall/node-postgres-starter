58d3d40b3c28104193bcdb8b4b01b42cfcf77a06
Dallas Hall <dallas.hall@gmail.com>
2017-06-14 23:52:39 -0700

    Node Postgres Starter

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', (req, res) => res.json({
+  message: 'Not much to see here.',
+}));
+
+module.exports = app;
